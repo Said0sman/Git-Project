cd592a0e328ce75f04548fe6a89d6e2173a63d56
Said Osman <said_henry@hotmail.com>
2021-05-03 01:47:56 +0200

    import robots.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'tachyons'
+import 'tachyons';
+import {robots} from './global/robots';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
