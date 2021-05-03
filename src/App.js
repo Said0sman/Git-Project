6bba3844200db0e3f772f9f52eacbbf9b9019a58
Said Osman <said_henry@hotmail.com>
2021-05-04 00:56:02 +0200

    Refactor not used code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import Card from "./components/Cards";
+//import Card from "./components/Cards";
+import CardList from "./components/CardList";
 
 function App({ robots }) {
   return (
     //step1()
-    step2(robots)
+    //step2(robots)
+    step3(robots)
   );
 }
 
 export default App;
 
-function step1() {
+/*function step1() {
   return (
     <>
       <Card id="1" name="John Doe" email="john.doe@gmail.com" />
@@ -28,4 +30,10 @@ function step2(robots) {
       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
     </>
   );
+}*/
+
+function step3(robots) {
+return (
+  <CardList robots={robots}/>
+)
 }
